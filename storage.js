b8c534688f72f2b6a8bb218377ccaec28c95e310
Alex Sopinka <alex@tasytt.com>
2017-09-28 14:21:54 -0400

    Redis capability + version update to 2.0.0

--- storage.js
+++ storage.js
@@ -1,5 +1,7 @@
 var Store = require("jfs");
 var db = require('monk');
+var redis = require('redis');
+
 var uuid = require('uuid/v4');
 
 module.exports = function(config) {
@@ -7,15 +9,16 @@ module.exports = function(config) {
 		throw new Error('You must pass a config to botkit-interactive-storage');
 	}
 
-	if (!config.mongo_uri && !config.path) {
-		throw new Error('You must pass a mongo_uri or path property');
+	if (!config.mongo_uri && !config.path && !config.redis_uri) {
+		throw new Error('You must pass a mongo_uri, redis_uri, or path property');
 	}
 
-	if (config.mongo_uri && config.path) {
-		throw new Error('Please pass a mongo_uri OR path property, not both')
+	if ((config.mongo_uri && config.path) || (config.redis_uri && config.path)) {
+		throw new Error('Please pass 1 of mongo_uri, redis_uri, or path property.');
 	}
 
 	var isLocal = config.path != null;
+	var isMongo = config.mongo_uri != null;
 
 	var unwrapFromList = function(cb) {
 		return function(err, data) {
@@ -31,8 +34,13 @@ module.exports = function(config) {
 	if (isLocal) {
 		msg_db = new Store(config.path + "/interactive-storage", { saveId: 'id' });
 	}
-	else {
+	else if (isMongo) {
 		msg_db = db(config.mongo_uri).get('interactive-storage');
+	}
+	else {
+		msg_db = redis.createClient({
+			url: config.redis_uri
+		});
 	}
 
 	var messages = {
@@ -44,10 +52,15 @@ module.exports = function(config) {
 			if (isLocal) {
 				msg_db.save(msg.id, msg, cb);
 			}
-			else {
+			else if (isMongo) {
 				msg_db.insert(msg, function(err, result) {
 					cb(null, result.id);
 				});
+			}
+			else {
+				msg_db.set(msg.id, msg, function(err, result) {
+					cb(null, result.id);
+				})
 			}
 		},
 		get: function(id, cb) {
@@ -58,12 +71,17 @@ module.exports = function(config) {
 					});
 				});
 			}
-			else {
+			else if (isMongo) {
 				msg_db.find({ id: id }, unwrapFromList(function(err, result) {
 					msg_db.remove({ id: id }, function(err) {
 						cb(null, result);
 					});
 				}));
+			}
+			else {
+				msg_db.get(msg.id, function(err, result) {
+					cb(null, result);
+				});
 			}
 		}
 	};
